9526b420267ca165bd5a2d0b5e7f0d3cd032be3a
Dhananjeyan Balaretnaraja <sdbala@gmail.com>
2024-06-05 11:25:47 +0530

    Moved all polly policies to an extension class. Cleaned Up Program.cs

--- OrderingService/Program.cs
+++ OrderingService/Program.cs
@@ -1,44 +1,13 @@
 // OrderingService/Program.cs
 
-using System.Net;
-using Polly;
-using Polly.Extensions.Http;
-
 var builder = WebApplication.CreateBuilder(args);
 
-// Define Polly policies
-var retryPolicy = HttpPolicyExtensions
-    .HandleTransientHttpError()
-    .RetryAsync(3);
+var services = builder.Services;
 
-var circuitBreakerPolicy = HttpPolicyExtensions
-    .HandleTransientHttpError()
-    .CircuitBreakerAsync(5, TimeSpan.FromSeconds(30));
 
-var timeoutPolicy = Policy.TimeoutAsync<HttpResponseMessage>(5);
-
-var bulkheadPolicy = Policy.BulkheadAsync<HttpResponseMessage>(10, 20);
-
-var fallbackPolicy = Policy<HttpResponseMessage>
-    .Handle<Exception>()
-    .FallbackAsync(new HttpResponseMessage(HttpStatusCode.OK)
-    {
-        Content = new StringContent("Fallback response")
-    });
-
-builder.Services.AddHttpClient("InventoryClient", client =>
-    {
-        client.BaseAddress = new Uri("http://localhost:5000/api/inventory"); // Replace with actual URL
-    })
-    .AddPolicyHandler(retryPolicy)
-    .AddPolicyHandler(circuitBreakerPolicy)
-    .AddPolicyHandler(timeoutPolicy)
-    .AddPolicyHandler(bulkheadPolicy)
-    .AddPolicyHandler(fallbackPolicy);
-
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+services.AddControllers();
+services.AddEndpointsApiExplorer();
+services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -47,4 +16,4 @@ app.UseSwaggerUI();
 
 app.MapControllers();
 
-app.Run();
+await app.RunAsync().ConfigureAwait(false);
